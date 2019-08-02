0d75e3f12adeb2adf5cf058f053cc5b081038a5f
daniel <dan137clarke@gmail.com>
2019-08-02 19:37:02 +1000

    game canvas now maintains aspect ratio on resize

--- src/render-handler.js
+++ src/render-handler.js
@@ -15,10 +15,30 @@ export default class RenderHandler {
         return this.layerMap[layerName];
     }
     render() {
+        let sHeight = this.layerMap[this.layerNames[0]].height;
+        let sWidth = this.layerMap[this.layerNames[0]].width;
+        let dHeight = sHeight;
+        let dWidth = sWidth;
+        let heightRatio = this.canvas.height / sHeight;
+        let widthRatio = this.canvas.width / sWidth;
+        let dx = 0;
+        let dy = 0;
+        if (widthRatio < heightRatio) {
+            dWidth = widthRatio * sWidth;
+            dHeight = widthRatio * sHeight;
+        }
+        else {
+            dWidth = heightRatio * sWidth;
+            dHeight = heightRatio * sHeight;
+        }
+        dx = (this.canvas.width - dWidth) / 2;
+        dy = (this.canvas.height - dHeight) / 2;
         let context = this.canvas.getContext('2d');
         if (context) {
+            context.fillStyle = "#000000";
+            context.fillRect(0, 0, this.canvas.width, this.canvas.height);
             for (let layerName of this.layerNames) {
-                context.drawImage(this.layerMap[layerName], 0, 0, this.canvas.width, this.canvas.height);
+                context.drawImage(this.layerMap[layerName], 0, 0, sWidth, sHeight, dx, dy, dWidth, dHeight);
             }
         }
         return this.canvas;
